e07ac266998765f582384b025c7218d840554c56
Kenaz Chan <kenazczx@gmail.com>
2025-03-31 23:48:26 +0800

    Some code added

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request, redirect
-from fetch_restaurants import fetch_restaurants
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask_session import Session
+from cs50 import SQL
+from geopy.geocoders import Nominatim
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Route for the home page
 @app.route('/')
